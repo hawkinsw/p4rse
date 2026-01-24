995abcc4706dcf05e3437fe6d04b2648fea48305
Will Hawkins <hawkinsw@obs.cr>
2026-01-23 23:51:49 -0500

    Add License Information
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- Sources/P4/Types.swift
+++ Sources/P4/Types.swift
@@ -1,5 +1,19 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// p4rse, Copyright 2026, Will Hawkins
+//
+// This file is part of p4rse.
+
+// This file is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 public enum ValueType: CustomStringConvertible {
     case Boolean(Bool)
