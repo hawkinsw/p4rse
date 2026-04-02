fc0276ad4241f621fe953cdfd74417bd71c8fde4
Will Hawkins <hawkinsw@obs.cr>
2026-04-02 02:48:43 -0400

    Support Parsing Function Declarations
    Signed-off-by: Will Hawkins <hawkinsw@obs.cr>

--- tree-sitter-p4/grammar.js
+++ tree-sitter-p4/grammar.js
@@ -64,11 +64,13 @@ export default grammar({
         instantiation: $ => seq($.typeRef, '(', optional($.parameter_list), ')', $.identifier),
 
         // Declarations
-        declaration: $ => seq(choice($.parserDeclaration, $.parserTypeDeclaration, $.type_declaration)),
+        declaration: $ => seq(choice($.parserDeclaration, $.parserTypeDeclaration, $.type_declaration, $.function_declaration)),
 
         type_declaration: $=> choice($.struct_declaration),
         struct_declaration: $ => seq($.struct, $.identifier, '{', optional($.struct_declaration_fields), '}'),
         struct_declaration_fields: $=> repeat1(seq($.variableDeclaration)),
+
+        function_declaration: $=> seq($.typeRef, $.identifier, $.parameters, $.statement),
 
         // Make separate productions for the parser type and the parser type declaration because the latter can have type parameters.
         parserTypeDeclaration: $ => seq(optional($.annotations), $.parser, field('parser_name', $.identifier), optional($.typeParameters), $.parameters),
